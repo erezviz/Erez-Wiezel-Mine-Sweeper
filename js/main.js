eb36847832061a46fb825edbd0c3fe2e57226141
Erez Wiezel <99121505+erezviz@users.noreply.github.com>
2022-03-24 19:20:43 +0200

    second-commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ const EMPTY = ''
 const GAME_OVER = '☠️'
 const GAME_WON = '😎'
 const GAME_ON = '😃'
-var gTimerIntervalId
+
+
+var gIntervalId = undefined
 var gBoard
 var gGame = {
     isOn: false,
@@ -16,80 +18,83 @@ var gGame = {
 }
 var gLevel = {
         SIZE: 4,
-        MINES: 1
+        MINES: 2
     }
     //TODO  -------------$  INITIALIZE GAME  $--------------
 function initGame() {
-    if (gTimerIntervalId) clearInterval(gTimerIntervalId)
+
+    clearInterval(gIntervalId)
+
 
     gBoard = createBoard(gLevel.SIZE)
-    getminesRandom(gLevel.MINES)
+    getMinesRandom(gLevel.MINES)
 
 
     printBoard(gBoard)
     renderBoard(gBoard, '.board-container')
     printBoard(gBoard)
+    console.log(gLevel.MINES);
+
+}
+
+//TODO -------  LEVEL SELECTOR  ----------
+
+function levelSelector(level) {
+    if (level === gLevel.SIZE) return
+    if (gGame.isOn && gGame.secsPassed) return
+    switch (level) {
+        case 4:
+            gLevel.SIZE = 4
+            gLevel.MINES = 2
+            break
+        case 8:
+            gLevel.SIZE = 8
+            gLevel.MINES = 12
+            break
+        case 12:
+            gLevel.SIZE = 12
+            gLevel.MINES = 32
+            break
+
+    }
+
+    initGame()
 
 }
 
 
-//TODO ---------- 
-// first test: we have a mat!
-// var test = createBoard(gLevel.SIZE)
-// console.log('expect a 4x4 table of objects\t');
-// console.table(test)
-//TODO ---------- CHECK WIN ------------
+//TODO ----------$ CHECK WIN $------------
 function checkWin() {
-    // var elMines = document.querySelectorAll('.mine')
-    // var minesFlagged = 0
-    // for(var i = gLevel.MINES; i>0; i--){
-    //     if(elMines.classList.contains('flagged')){
-    //         minesFlagged++
-    //     }
-    // }
-    console.log('gGame.markedCount', gGame.markedCount);
-    console.log('gLevel.MINES', gLevel.MINES);
-    console.log('gLevel.SIZE x gLevel.SIZE', gLevel.SIZE * gLevel.SIZE);
-    console.log('shownCount', gGame.shownCount);
+    console.log('marked', gGame.markedCount);
+    console.log('shown', gGame.shownCount);
+    console.log('glevel size squared - mines', (gLevel.SIZE * gLevel.SIZE) - gLevel.MINES);
     if (gGame.markedCount === gLevel.MINES &&
         (gLevel.SIZE * gLevel.SIZE) - gLevel.MINES === gGame.shownCount) {
-        clearInterval(gTimerIntervalId)
+        clearInterval(gIntervalId)
+        gGame.isOn = false
         document.querySelector('.game-btn').innerText = GAME_WON
-        console.log('you Won!');
+
+
     }
 }
 //TODO ------------ RESTART GAME  -----------------
 function restartGame(elBtn) {
+
     elBtn.innerText = GAME_ON
+
+    clearInterval(gIntervalId)
+
     gGame.isOn = false
     gGame.secsPassed = 0
     gGame.markedCount = 0
     gGame.shownCount = 0
+    document.querySelector('.marked-count').innerText = gGame.markedCount
+    document.querySelector('.timer').innerText = gGame.secsPassed
     initGame()
-
-
-
-
-}
-//TODO -----------  PRINT BOARD  (FOR CONSOLE USE)---------------
-function printBoard(board) {
-    var boardSym = []
-    for (var i = 0; i < board.length; i++) {
-        boardSym[i] = []
-        for (var j = 0; j < board.length; j++) {
-
-            boardSym[i][j] = board[i][j].symbol
-
-        }
-
-    }
-    console.table(boardSym)
 }
 
-// second test: we have mine neighbors!!
-// var test = createBoard(gLevel.SIZE)
-// console.log('expected: 0,1;1,0;1,1 have minesaroundcount of: 2. Actual: \t');
-// console.table(test)
+
+
 //TODO  -------------$  CREATE BOARD  $--------------
 function createBoard(SIZE) {
 
@@ -100,60 +105,38 @@ function createBoard(SIZE) {
         for (var j = 0; j < SIZE; j++) {
 
             board[i][j] = {
-                minesAroundCount: 0,
-                isShown: false,
-                isMine: false,
-                isMarked: false,
-                symbol: EMPTY
-            }
-            var cell = board[i][j]
+                    minesAroundCount: 0,
+                    isShown: false,
+                    isMine: false,
+                    isMarked: false,
+                    symbol: EMPTY
+                }
+                // var cell = board[i][j]
 
+            // var negsMineCount = setMineNegsCount(i, j, board)
 
+            // if (negsMineCount !== 0 && !cell.isMine) {
 
+            //     cell.minesAroundCount = negsMineCount
+            //     cell = negsMineCount
+            // } else if (negsMineCount === 0 && !cell.isMine) {
 
-            var negsMineCount = setMineNegsCount(i, j, board)
+            //     cell = EMPTY
+            // }
+            // if (cell.isMine) {
 
-            if (negsMineCount !== 0 && !cell.isMine) {
-
-                cell.minesAroundCount = negsMineCount
-                cell = negsMineCount
-            } else if (negsMineCount === 0 && !cell.isMine) {
-
-                cell = EMPTY
-            }
-            if (cell.isMine) {
-
-                console.log('in mine if');
-                cell.minesAroundCount = negsMineCount
-
-                cell = MINE
-
-            }
+            //     cell.minesAroundCount = negsMineCount
+            //     cell = MINE
+            // }
         }
 
 
     }
 
-
-
-
-    // console.table(board)
     return board
 }
 
-//TODO  -------------$  GET MINES RANDOM  $--------------
-function getminesRandom(mineNum) {
 
-    for (var i = mineNum; i > 0; i--) {
-
-        var randIdxI = getRandomInt(0, gBoard.length - 1)
-        var randIdxj = getRandomInt(0, gBoard.length - 1)
-        gBoard[randIdxI][randIdxj].isMine = true
-        gBoard[randIdxI][randIdxj].symbol = MINE
-
-    }
-
-}
 
 //TODO  -------------$  SET MINE NEIGHBORS COUNT  $--------------
 function setMineNegsCount(cellI, cellJ, board) {
@@ -172,17 +155,6 @@ function setMineNegsCount(cellI, cellJ, board) {
 
     return mineNegsCount
 }
-//TODO  --------------  COPY BOARD  ---------------
-function copyBoard(board) {
-    var newBoard = [];
-    for (var i = 0; i < board.length; i++) {
-        newBoard[i] = [];
-        for (var j = 0; j < board[0].length; j++) {
-            newBoard[i][j] = board[i][j];
-        }
-    }
-    return newBoard;
-}
 
 //TODO  -------------$  RENDER BOARD  $--------------
 function renderBoard(board, selector) {
@@ -190,7 +162,7 @@ function renderBoard(board, selector) {
     var strHTML = `<table><tbody>`;
     for (var i = 0; i < board.length; i++) {
 
-        strHTML += `<tr>`
+        strHTML += `<tr class="row">`
         for (var j = 0; j < board[i].length; j++) {
             var cell = board[i][j]
             var cellTxt = EMPTY
@@ -221,7 +193,7 @@ function renderBoard(board, selector) {
             className += hidden
 
 
-            strHTML += `<td onmouseup="cellMarked(event,this)"onclick="cellClicked(this,${i},${j})" class="${className}" >${cellTxt}</td>`
+            strHTML += `<td data-i="${i}" data-j="${j}" onmouseup="cellMarked(event,this,${i},${j})"onclick="cellClicked(this,${i},${j})" class="${className}" >${cellTxt}</td>`
         }
         strHTML += `</tr>`
     }
@@ -230,78 +202,95 @@ function renderBoard(board, selector) {
     elContainer.innerHTML = strHTML
 }
 
+
+
 //TODO  -------------$  CELL CLICKED  $--------------
 function cellClicked(elCell, cellI, cellJ) {
-    if (gBoard[cellI][cellJ].isShown) return
-    if (gBoard[cellI][cellJ].isMarked) return
-    if (elCell.classList.contains('flagged')) return
     if (gGame.shownCount === 0) gGame.isOn = true
-    if (gBoard[cellI][cellJ].isMine) {
-        gameOver()
-        gGame.isOn = false
-    }
-    gameTimer()
-    gGame.shownCount++
+
+    if (gBoard[cellI][cellJ].isShown) return
+
+    if (gBoard[cellI][cellJ].isMarked) return
+
+    if (elCell.classList.contains('flagged')) return
+
+    if (gGame.isOn) {
+
+        gGame.shownCount++
+
+            if (gBoard[cellI][cellJ].isMine) {
+                gameOver()
+
+
+            }
+        if (elCell.innerText === EMPTY) {
+            console.log('im empty');
+
+            var negsBlown = expandEmptyNegs(gBoard, cellI, cellJ)
+            gGame.shownCount += negsBlown
+            console.log('negs blown', negsBlown);
+        }
+        if (gGame.isOn && gGame.shownCount === 1) {
+            console.log('in timer interval');
+            gIntervalId = setInterval(gameTimer, 1000)
+        }
+
+
+        gBoard[cellI][cellJ].isShown = true
+        elCell.classList.remove('hidden')
         checkWin()
-    console.log('gGame.shownCount in cellClicked', gGame.shownCount);
-    gBoard[cellI][cellJ].isShown = true
-    elCell.classList.remove('hidden')
+    } else {
+        return
+    }
+
 
 
 }
 //TODO  -------------  TIMER  --------------
 function gameTimer() {
-    if (gTimerIntervalId) return
-    if (gGame.isOn) {
 
-
-        gTimerIntervalId = setInterval(function() {
-
-            document.querySelector('.timer').innerText = ++gGame.secsPassed
-
-        }, 1000);
-    }
+    document.querySelector('.timer').innerText = ++gGame.secsPassed
 }
 
 //TODO  -------------  CELL MARKED  --------------
 function cellMarked(ev, elCell) {
     elCell.addEventListener("contextmenu", ev => ev.preventDefault())
 
-    if (ev.button === 2) {
 
-        elCell.classList.toggle('flagged')
-        if (elCell.classList.contains('flagged')) {
-            gGame.markedCount++
+
+    if (gGame.isOn) {
+
+        if (ev.button === 2) {
+
+            elCell.classList.toggle('flagged')
+            if (elCell.classList.contains('flagged')) {
+                gGame.markedCount++
+                    gBoard[+elCell.dataset.i][+elCell.dataset.j].isMarked = true
+
+                document.querySelector('.marked-count').innerText = gGame.markedCount
                 checkWin()
-        } else {
-            gGame.markedCount--
+            } else {
+                gGame.markedCount--
+                    document.querySelector('.marked-count').innerText = gGame.markedCount
+                gBoard[+elCell.dataset.i][+elCell.dataset.j].isMarked = false
                 checkWin()
+            }
         }
     }
-    // console.log(gGame.markedCount);
+
 
 }
 
 //TODO ----------- GAME OVER ----------------
 function gameOver() {
-    console.log(gTimerIntervalId);
-    clearInterval(gTimerIntervalId)
+    clearInterval(gIntervalId)
+    gGame.isOn = false
     var elMines = document.querySelectorAll('.mine')
     document.querySelector('.game-btn').innerText = GAME_OVER
     for (var i = 0; i <= gLevel.MINES; i++) {
         elMines[i].classList.remove('hidden')
-        console.log(elMines[i].classList.remove('hidden'));
+
 
     }
 
-}
-
-
-
-//!  ------   GET RANDOM INT   ------------
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-    //The maximum is exclusive and the minimum is inclusive
 }
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,5 @@
 'use strict'
+
 //!  ------   GET RANDOM INT   ------------
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -8,187 +9,61 @@ function getRandomInt(min, max) {
 }
 
 
-//!  ------   GET RANDOM INT INCLUSIVE  ------------
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-    //The maximum is inclusive and the minimum is inclusive
-}
+//TODO  --------------$ EXPAND EMPTY NEIGHBORS $--------------- 
+function expandEmptyNegs(board, cellI, cellJ) {
+
+    var negsBlownCount = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= board[i].length) continue
+            if (board[i][j].isMarked) continue
+            if (board[i][j].isMine) continue
+            if (!board[i][j].isShown) {
+
+                board[i][j].isShown = true
+                negsBlownCount++
 
 
-//!  ------   GET RANDOM COLOR  ------------
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-} // function returns a random color. Can be used for any css color aspect (i.e. BG, color)
-
-
-//!  ------   GET SUM COLS  ------------
-function getSumCols(mat, colIdx) {
-    var sumOfCols = 0
-    for (var i = 0; i < mat.length; i++) {
-        sumOfCols += mat[i][colIdx]
-    } // function returns the sum of the column by a a given column index. 
-    return sumOfCols
-
-}
-
-//!  ------   GET SUM ROW  ------------
-function sumRow(mat, rowIdx) {
-    var rowToSum = mat[rowIdx]
-    var sumOfRows = 0
-    for (var i = 0; i < rowToSum.length; i++) {
-        sumOfRows += rowToSum[i]
-
-    }
-    return sumOfRows
-        // function returns the sum of the row by the given index
-}
-
-
-//!  ------   IS MAT SYMMETRIC  ------------
-function isMatSymmetric(mat) {
-    for (var i = 0; i < mat.length - 1; i++) {
-        for (var j = i + 1; j < mat[i].length; j++) {
-
-            if (mat[i][j] !== mat[j][i]) return false
-        }
-    }
-    return true
-        // function returns boolean in regard to the symmetry of given mat.
-}
-
-
-
-//!  ------   DRAW NUM  ------------
-
-//!   NOTE: THIS FUNCTION REQUIRES GET RANDOM INT FUNCTION AS WELL
-function drawNum(nums) {
-
-    var idx = getRandomInt(0, nums.length)
-    var num = nums[idx]
-    nums.splice(idx, 1)
-        // THE SAME
-        // var num = nums.splice(idx, 1)[0]
-    return num
-        // function draws a number randomely from an array of numbers, 
-        // then splices it out, so as not to use it again. 
-}
-
-//!  ------   GET MAT \ CREATE BOARD ------------
-function getMat(rows, cols) {
-    var mat = []
-    for (var i = 0; i < rows; i++) {
-        mat[i] = []
-        for (var j = 0; j < cols; j++) {
-            mat[i][j] = i + ',' + j //  <= This must be changed to work with your code correctly
-        }
-    }
-    return mat
-} // function returns a 5x5 matrice (easily changed), 
-
-//[[0,0],[0,1],[0,2],
-//[[1,0],[1,1],[1,2],
-//[[2,0],[2,1],[2,2]
-
-//?  ------   PRINT BOARD------------ 
-// function renderBoard(board, selector) {
-//     var strHTML = '<table border="0"><tbody>';
-//     for (var i = 0; i < board.length; i++) {
-//         strHTML += '<tr>'
-//         for (var j = 0; j < board[0].length; j++) {
-//             var cell = board[i][j].minesAroundCount
-//             var className = `cell cell-${i}-${j}`
-//             strHTML += `<td onclick="cellClicked(this,${i},${j})" class="${className}" >${cell}</td>`
-//         }
-//         strHTML += '</tr>'
-//     }
-//     strHTML += '</tbody></table>'
-//     var elContainer = document.querySelector(selector)
-//     elContainer.innerHTML = strHTML
-// } // Function prints a matrix to the DOM as a table with tbody and td cells. 
-//  func param: mat --> 
-
-//!  ------   GET EMPTY CELLS ------------
-function getEmptyCells(board) {
-    var cells = [];
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < board[0].length; j++) {
-            var cell = board[i][j];
-            if (!cell.gameElement && cell.type === FLOOR) {
-                cells.push({ i, j });
+                document.querySelector(`.cell-${i}-${j}`).classList.remove('hidden')
             }
+
+
         }
+
     }
-    return cells;
+    return negsBlownCount
 }
 
 
-// TODO -------------   RENDER BOARD  $$$NEEDS CHANGING$$$ ------------------
-function renderBoard(board, selector) {
 
-    var strHTML = ''
+//TODO  -------------$  GET MINES RANDOM  $--------------
+function getMinesRandom(mineNum) {
+
+    for (var i = 0; i < mineNum; i++) {
+
+        var randIdxI = getRandomInt(0, gBoard.length)
+        var randIdxj = getRandomInt(0, gBoard.length)
+        gBoard[randIdxI][randIdxj].isMine = true
+        gBoard[randIdxI][randIdxj].symbol = MINE
+        console.log(i);
+
+    }
+
+}
 
 
+
+//TODO -----------  PRINT BOARD  (FOR CONSOLE USE)---------------
+function printBoard(board) {
+    var boardSymbolValues = []
     for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>'
+        boardSymbolValues[i] = []
         for (var j = 0; j < board.length; j++) {
-            // var cell = drawNum(gNums)
 
-            strHTML += `<td onclick="cellClicked(this,${i},${j})" class="cell" ></td>`
-
-        }
-        strHTML += '</tr>'
-    }
-
-    var elBoard = document.querySelector(`.${selector}`)
-    elBoard.innerHTML = strHTML
-}
-
-
-//TODO  -------------- COUNT NEIGHBORS ---------------  
-function countNeighbors(cellI, cellJ, mat) {
-    var neighborsCount = 0;
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue;
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue;
-            if (j < 0 || j >= mat[i].length) continue;
-
-            if (mat[i][j] === LIFE || mat[i][j] === SUPER_LIFE) neighborsCount++;
+            boardSymbolValues[i][j] = board[i][j].symbol
         }
     }
-    return neighborsCount;
-}
-
-//TODO  -------------- COPY MAT ---------------  
-function copyMat(mat) {
-    var newMat = [];
-    for (var i = 0; i < mat.length; i++) {
-        newMat[i] = [];
-        for (var j = 0; j < mat[0].length; j++) {
-            newMat[i][j] = mat[i][j];
-        }
-    }
-    return newMat;
-} // function returns a copy of the sent mat
-
-
-//TODO  -------------- FIND NEIGHBORS --------------- 
-function findNeighbors(mat, cellI, cellJ) {
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue;
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue;
-            if (j < 0 || j >= mat[i].length) continue;
-
-            // Write here code for action on neighbors
-        }
-
-    }
+    console.table(boardSymbolValues)
 }
